e986c1d8cca101737bd9752788cbb5a20e8023f8
dineshwasti <65214801+dineshwasti@users.noreply.github.com>
2023-11-23 13:55:46 +0545

    farmer.jsx modified

--- final-project/src/pages/Farmer.jsx
+++ final-project/src/pages/Farmer.jsx
@@ -1,19 +1,3 @@
-// Farmer.js
-
-import React from 'react';
-
-const Farmer = () => {
-  // Your component logic here
-  return (
-    <div>
-      <h2>Farmer Page</h2>
-      {/* Add your farmer-specific content here */}
-    </div>
-  );
-};
-
-export default Farmer;
-
 import React from 'react'
 
 export const Farmer = () => {
